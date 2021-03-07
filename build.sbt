e422806b16d75f9cee29653aeef062d6e287eeee
Ralf Gueldemeister <rgueldemeister@zendesk.com>
2021-03-07 14:21:10 -0700

    Replace log parser with http4s service

--- build.sbt
+++ build.sbt
@@ -2,12 +2,19 @@ name := "intro-to-scala"
 
 version := "0.1"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % Test
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.0" % Test,
+  "org.http4s" %% "http4s-dsl" % "0.21.20",
+  "org.http4s" %% "http4s-blaze-server" % "0.21.20",
+)
 
 scalaVersion := "2.13.3"
 
 scalacOptions ++= Seq(
-  "-Werror"
+  "-Werror",
+  "-deprecation"
 )
 
-testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-C", "delight.LittleRed")
+Test / testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-C", "delight.LittleRed")
+
+Compile / run / fork := true
---
+++ src/test/scala/introcourse/level07/ServiceTest.scala
@@ -0,0 +1,69 @@
+package introcourse.level07
+
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.funspec.AsyncFunSpec
+import org.http4s.HttpApp
+import org.http4s.Request
+import org.http4s.Uri
+import cats.effect.IO
+
+class ServiceTest extends AsyncFunSpec with TypeCheckedTripleEquals {
+
+  describe("userNames") {
+    it("should return user names, separated by newlines") {
+      val usernames = Service.userNames(List(User("Vincent", 30), User("Jules", 35)))
+      assert(usernames === "Vincent\nJules")
+    }
+
+    it("should handle empty lists") {
+      val usernames = Service.userNames(List())
+      assert(usernames === "")
+    }
+  }
+
+  describe("GET /ping") {
+    it("should respond with pong") {
+      val service = HttpApp(Service.run)
+      val request = Request[IO](uri = Uri(path = "/ping"))
+
+      service(request)
+        .flatMap(response => response.as[String])
+        .map(body => assert(body === "pong"))
+        .unsafeToFuture()
+    }
+  }
+
+  describe("GET /users") {
+    it("should respond with a list of user names") {
+      val service = HttpApp(Service.run)
+      val request = Request[IO](uri = Uri(path = "/users"))
+
+      service(request)
+        .flatMap(response => response.as[String])
+        .map(body => assert(body === "Vincent\nJules\nMia\nWinston"))
+        .unsafeToFuture()
+    }
+  }
+
+  describe("GET /unknown") {
+    it("should respond with 404") {
+      val service = HttpApp(Service.run)
+      val request = Request[IO](uri = Uri(path = "/unknown"))
+
+      service(request)
+        .map(response => assert(response.status.code === 404))
+        .unsafeToFuture()
+    }
+  }
+
+  describe("usersWithLog") {
+    it("should respond with 200") {
+      val request = Request[IO](uri = Uri(path = "/unknown"))
+      val response = Service.usersWithLog(request)
+
+      response
+        .map(response => assert(response.status.code === 200))
+        .unsafeToFuture()
+    }
+  }
+}
